a167354459a8a45243b6828dceffc3490ee32142
sian.izm <sian.izm@gmail.com>
2022-01-04 23:05:26 +0700

    define various routes

--- src/cats/cats.controller.ts
+++ src/cats/cats.controller.ts
@@ -1,10 +1,24 @@
-import { Controller, Get, Req } from '@nestjs/common';
+import { Controller, Get, HttpCode, Post, Redirect, Param, Query} from '@nestjs/common';
 import { Request } from 'express';
 
 @Controller('cats')
 export class CatsController {
   @Get()
-  findAll(@Req() request: Request): string {
-    return 'This action returns all cats';
+  @Redirect('https://nestjs.com', 301)
+  getDocs(@Query('version') version) {
+    if (version && version === '5') {
+      return { url: 'https://docs.nestjs.com/v5/' };
+    }
+  }
+
+  @Get(':id')
+  findOne(@Param('id') id: string): string {
+  return `This action returns a #${id} cat`;
+  }
+
+  @Post()
+  @HttpCode(204)
+  create(): string {
+    return 'This action adds a new cat';
   }
 }
