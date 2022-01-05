00e1307454d66316f6420de3861223ebd0a599a0
sian.izm <sian.izm@gmail.com>
2022-01-05 20:29:39 +0700

    create service

--- src/cats/cats.controller.ts
+++ src/cats/cats.controller.ts
@@ -1,11 +1,15 @@
 import { Controller, Get, HttpCode, Post, Param, Body } from '@nestjs/common';
 import { CreateCatDto } from './create-cat.dto';
+import { CatsService } from './cats.service';
+import { Cat } from './interfaces/cat.interface';
 
 @Controller('cats')
 export class CatsController {
+  constructor(private catsService: CatsService) {}
+
   @Get()
-  async findAll(): Promise<any[]> {
-    return [];
+  async findAll(): Promise<Cat[]> {
+    return this.catsService.findAll();
   }
 
   @Get(':id')
@@ -16,6 +20,7 @@ export class CatsController {
   @Post()
   @HttpCode(204)
   async create(@Body() createCatDto: CreateCatDto) {
-    return 'This action adds a new cat';
+    console.log(createCatDto);
+    this.catsService.create(createCatDto);
   }
 }
