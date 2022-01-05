6caf73538f0638fa8839c2be85028af166bded70
sian.izm <sian.izm@gmail.com>
2022-01-05 19:44:51 +0700

    added dto for create

--- src/cats/cats.controller.ts
+++ src/cats/cats.controller.ts
@@ -1,14 +1,11 @@
-import { Controller, Get, HttpCode, Post, Redirect, Param, Query} from '@nestjs/common';
-import { Request } from 'express';
+import { Controller, Get, HttpCode, Post, Param, Body } from '@nestjs/common';
+import { CreateCatDto } from './create-cat.dto';
 
 @Controller('cats')
 export class CatsController {
   @Get()
-  @Redirect('https://nestjs.com', 301)
-  getDocs(@Query('version') version) {
-    if (version && version === '5') {
-      return { url: 'https://docs.nestjs.com/v5/' };
-    }
+  async findAll(): Promise<any[]> {
+    return [];
   }
 
   @Get(':id')
@@ -18,7 +15,7 @@ export class CatsController {
 
   @Post()
   @HttpCode(204)
-  create(): string {
+  async create(@Body() createCatDto: CreateCatDto) {
     return 'This action adds a new cat';
   }
 }
