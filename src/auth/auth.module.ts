365123be7cc012a22273160cff3b5864c0093517
sian.izm <sian.izm@gmail.com>
2022-01-29 22:30:48 +0700

    devide secret key to constnat file

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -4,13 +4,14 @@ import { UsersModule } from '../users/users.module';
 import { PassportModule } from '@nestjs/passport';
 import { LocalStrategy } from './local.strategy';
 import { JwtModule } from '@nestjs/jwt';
+import { jwtConstants } from './constants';
 
 @Module({
   imports: [
     UsersModule,
     PassportModule,
     JwtModule.register({
-      secret: 'secret',
+      secret: jwtConstants.secret,
       signOptions: { expiresIn: '60s' },
     }),
   ],
---
+++ src/auth/constants.ts
@@ -0,0 +1,4 @@
+export const jwtConstants = {
+  // FIXME: environment variable
+  secret: 'secretKey',
+}
