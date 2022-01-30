fae0316cbdc517667e8a4b67b3adb6bc29c901a9
sian.izm <sian.izm@gmail.com>
2022-01-30 23:09:35 +0700

    implement auth guard for /prifile

--- src/auth/jwt.strategy.ts
+++ src/auth/jwt.strategy.ts
@@ -1,7 +1,6 @@
 import { Injectable } from "@nestjs/common";
 import { PassportStrategy } from "@nestjs/passport";
-import { ExtractJwt } from "passport-jwt";
-import { Strategy } from "passport-local";
+import { ExtractJwt, Strategy } from "passport-jwt";
 import { jwtConstants } from "./constants";
 
 @Injectable()
@@ -9,9 +8,9 @@ export class JwtStrategy extends PassportStrategy(Strategy) {
   constructor() {
     super({
       jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
-        ignoreExpiration: false,
-        secretOrKey: jwtConstants.secret,
-      });
+      ignoreExpiration: false,
+      secretOrKey: jwtConstants.secret,
+    });
   }
 
   async validate(payload: any) {
