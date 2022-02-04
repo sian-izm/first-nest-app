6bac2bc9840cad0ce156d97841352e6d3f63378a
sian.izm <sian.izm@gmail.com>
2022-02-04 22:38:19 +0700

    install bcrypt

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,5 @@
-import { NestFactory, HttpAdapterHost } from '@nestjs/core';
+import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { AllExceptionsFilter } from './all.exceptions.filter';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
